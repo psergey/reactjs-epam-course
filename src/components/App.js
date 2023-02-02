17d6151d6b8a5b0a7abe2209a779744644b557cc
Sergiy Parkhomenko <sergey.parkhomenko@hotmail.com>
2023-02-02 17:36:08 +0200

    no message

--- src/components/App.js
+++
@@ -1,9 +0,0 @@
-import Header from "./Header";
-
-const App = () => {
-    return <div>
-        <Header message="React Application" />
-    </div>
-}
-
-export default App;
--- src/components/Header.tsx
+++
@@ -1,9 +0,0 @@
-import { FC } from 'react';
-
-const Header : FC<{message: string}> = ({message}): JSX.Element => {
-    return (
-        <h1>Hello {message}!</h1>
-    )
-}
-
-export default Header;
