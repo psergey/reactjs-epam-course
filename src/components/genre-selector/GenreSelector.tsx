11b9f383fa00c8a759e53d2f1bdd5294d28ea980
Sergiy Parkhomenko <sergey.parkhomenko@hotmail.com>
2023-05-11 11:14:54 +0300

    added lint-staged

--- src/components/genre-selector/GenreSelector.tsx
+++ src/components/genre-selector/GenreSelector.tsx
@@ -9,6 +9,7 @@ interface GenreSelectorProps {
 
 const GenreSelector: FC<GenreSelectorProps> = ({ genres, currentGenre, onSelected }): ReactElement => {
   const [selectedGenre, setSelectedGenre] = useState(currentGenre);
+  const x = '';
 
   const onClickHandlder = (value: string): void => {
     onSelected(value);
